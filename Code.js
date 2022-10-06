11d44edc664e37c4b8277e8ee3015d335d81496b
Khurram <khurramworkspace@gmail.com>
2022-10-06 22:57:00 +0200

    Activating the sheet to read email addresses

--- Code.js
+++ Code.js
@@ -6,5 +6,7 @@ function sendMessages(){
    Logger.log(aliases); //returns the list of aliases you have
    Logger.log(aliases[1]);
    var done = "done";
-   
-}
+   var ss1 = SpreadsheetApp.openById("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").getSheetByName("tosend").activate(); //Add your own Google Sheet in openById function. 
+   var lr = ss1.getLastRow(); 
+   Logger.log(lr);
+}   
