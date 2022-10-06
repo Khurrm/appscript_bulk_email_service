3ace56ee83d6a3adad36ff0deec09d6b8043da42
Khurram <khurramworkspace@gmail.com>
2022-10-06 22:55:18 +0200

    Finally started writing code.

--- Code.js
+++ Code.js
@@ -1,5 +1,10 @@
-function myFunction() {
- 
-     Logger.log("First Step");
+function sendMessages(){  
+  
+   var draft = GmailApp.getDrafts()[0];
+   var aliases = GmailApp.getAliases()
+   Logger.log(draft);  
+   Logger.log(aliases); //returns the list of aliases you have
+   Logger.log(aliases[1]);
+   var done = "done";
+   
 }
-    
