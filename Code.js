083d4d80946993b9477efa136824c5425d84c184
Khurram <khurramworkspace@gmail.com>
2022-10-14 23:16:59 +0200

    cleanig up the code

--- Code.js
+++ Code.js
@@ -6,13 +6,12 @@ function sendMessages(){
    Logger.log(aliases); //returns the list of aliases you have
    Logger.log(aliases[1]);
    var done = "done";
-   var ss1 = SpreadsheetApp.openById("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").getSheetByName("tosend").activate(); //Add your own Google Sheet in openById function. 
+   var ss1 = SpreadsheetApp.openById("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").getSheetByName("tosendXXX").activate(); //Add your own Google Sheet in openById function. 
    var lr = ss1.getLastRow(); 
    Logger.log(lr);
    
    var quotaLeft = MailApp.getRemainingDailyQuota(); // to check the daily quota.
    Logger.log(quotaLeft);
-   
    
    for (var i = 2; i <=lr; i++){
    var interim = ss1.getRange(i,5).getValue(); 
@@ -30,26 +29,17 @@ function sendMessages(){
         serialnumber: ss1.getRange(i,6).getValue()
                 
       };
-   // var temple = HtmlService.createTemplateFromFile('htmlandroidsend');
    var temple = HtmlService.createTemplateFromFile('XXXSend.html'); //mention the name of the file as in Appscript
-   //var temple = HtmlService.createTemplateFromFile('26Nov');
    temple.candidate = candidate; 
    var message = temple.evaluate().getContent();
    var currentemail = ss1.getRange(i,4).getValue();   
    Logger.log(currentemail);
-   // var subjectLine = "Hi " + candidate.first_name + "- Please update your XXXXXXX";
    var subjectLine = "Hi " + candidate.first_name + " - you are XXXXXX";
    var messageBody = "Hi " + candidate.first_name;
-   
+  
    SpreadsheetApp.flush();
-   //MailApp.sendEmail({to:currentemail,subject:subjectLine,body:messageBody,htmlBody:message}); 
-   //GmailApp.sendEmail(currentemail, subjectLine,messageBody,{'from':aliases[4], name:"XXXX Team Message",htmlBody:message,noReply:true});
-   //GmailApp.sendEmail(currentemail, subjectLine,messageBody,{'from':aliases[2], name:"XXXXX Notification - Team XXXXX",htmlBody:message,noReply:true});
-   //MailApp.sendEmail(currentemail, subjectLine,messageBody,{noReply:true});
-   
-	GmailApp.sendEmail(currentemail, subjectLine,messageBody,{'from':aliases[1], name:"Notification - From Team XXXX",htmlBody:message});
+   GmailApp.sendEmail(currentemail, subjectLine,messageBody,{'from':aliases[1], name:"Notification - From Team XXXX",htmlBody:message});
     ss1.getRange(i,8).setValue(done);
-   
       
     }
     }  
