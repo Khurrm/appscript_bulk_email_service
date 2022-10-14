b65957ddc2834ec2c1a143866fec0f6ec3e0ba53
Khurram <khurramworkspace@gmail.com>
2022-10-14 23:21:09 +0200

    No Reply email method.
    This code is almost similar like the other, except the users get a no-reply email so they cannot send the emails or reply. It is mainly used for Notification purposes.

---
+++ Code_No_Reply_Method.js
@@ -0,0 +1,48 @@
+function sendMessages(){  
+  //Users will get a no replay corresponding email when they want to send messages. 
+   var draft = GmailApp.getDrafts()[0];
+   var aliases = GmailApp.getAliases()
+   Logger.log(draft);  
+   Logger.log(aliases); //returns the list of aliases you have
+   Logger.log(aliases[1]);
+   var done = "done";
+   var ss1 = SpreadsheetApp.openById("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").getSheetByName("tosendXXX").activate(); //Add your own Google Sheet in openById function. 
+   var lr = ss1.getLastRow(); 
+   Logger.log(lr);
+   
+   var quotaLeft = MailApp.getRemainingDailyQuota(); // to check the daily quota.
+   Logger.log(quotaLeft);
+   
+   for (var i = 2; i <=lr; i++){
+   var interim = ss1.getRange(i,5).getValue(); 
+   Logger.log(interim);
+   if(interim != "done")
+   
+   {
+   var candidate = 
+      {
+        assetvalue: ss1.getRange(i,1).getValue(),
+        first_name: ss1.getRange(i,2).getValue(),
+        last_name : ss1.getRange(i,3).getValue(),
+        currentemail: ss1.getRange(i,4).getValue(),
+        model: ss1.getRange(i,5).getValue(),
+        serialnumber: ss1.getRange(i,6).getValue()
+                
+      };
+   var temple = HtmlService.createTemplateFromFile('XXXSend.html'); //mention the name of the file as in Appscript
+   temple.candidate = candidate; 
+   var message = temple.evaluate().getContent();
+   var currentemail = ss1.getRange(i,4).getValue();   
+   Logger.log(currentemail);
+   var subjectLine = "Hi " + candidate.first_name + " - you are XXXXXX";
+   var messageBody = "Hi " + candidate.first_name;
+  
+   SpreadsheetApp.flush();
+  
+   GmailApp.sendEmail(currentemail, subjectLine,messageBody,{'from':aliases[2], name:"Team Voice Message",htmlBody:message,noReply:true});
+   //MailApp.sendEmail(currentemail, subjectLine,messageBody,{noReply:true});
+  ss1.getRange(i,8).setValue(done);
+      
+    }
+    }  
+}   
